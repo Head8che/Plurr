91b9fccde6e5a3cbcfa36601d915ccd5243a4a0c
Zubier Hagi <hagi@ualberta.ca>
2021-11-12 05:37:20 -0700

    init heroku deployment v.02 big fix

--- api/urls.py
+++ api/urls.py
@@ -1,48 +1,48 @@
-# from django.urls import path, re_path, include
-# from django.conf.urls import url
+from django.urls import path, re_path, include
+from django.conf.urls import url
 
-# from .views import index, authorView, followerView, postView, authView, inboxView, likeView, likedView, commentView
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import index, authorView, followerView, postView, authView, inboxView, likeView, likedView, commentView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
-# urlpatterns = [
-#     # Index
-#     path('', index.index, name="index"),
+urlpatterns = [
+    # Index
+    path('', index.index, name="index"),
     
-#     # Auth Endpoints
-#     path('author/login/', authView.LoginView.as_view(), name="login"),
-#     path('author/register/', authView.SignupView, name="register"),
+    # Auth Endpoints
+    path('author/login/', authView.LoginView.as_view(), name="login"),
+    path('author/register/', authView.SignupView, name="register"),
 
-#     # Author Endpoints
-#     path('authors/',authorView.AuthorList, name ='authorList'),
-#     path('author/<str:author_uuid>/', authorView.AuthorDetail, name="authorDetail"),
+    # Author Endpoints
+    path('authors/',authorView.AuthorList, name ='authorList'),
+    path('author/<str:author_uuid>/', authorView.AuthorDetail, name="authorDetail"),
 
-#     # Inbox Endpoint
-#     path('author/<str:author_uuid>/inbox/', inboxView.InboxList, name="inboxList"),
+    # Inbox Endpoint
+    path('author/<str:author_uuid>/inbox/', inboxView.InboxList, name="inboxList"),
 
-#     # Follower Endpoints
-#     path('author/<str:author_uuid>/followers/', followerView.FollowerList, name="followerList"),
-#     path('author/<str:author_uuid>/followers/<str:follower_uuid>/', followerView.FollowerDetail, name="followerDetail"),
+    # Follower Endpoints
+    path('author/<str:author_uuid>/followers/', followerView.FollowerList, name="followerList"),
+    path('author/<str:author_uuid>/followers/<str:follower_uuid>/', followerView.FollowerDetail, name="followerDetail"),
 
-#     # Post Endpoints
-#     # path('author/<str:authorID>/stream/', postView.getStreamPosts, name="streamPosts"),
-#     path('author/<str:author_uuid>/posts/', postView.PostList, name="authorPosts"),
-#     path('author/<str:author_uuid>/posts/<str:post_uuid>/', postView.PostDetail, name="authorPost"),
+    # Post Endpoints
+    # path('author/<str:authorID>/stream/', postView.getStreamPosts, name="streamPosts"),
+    path('author/<str:author_uuid>/posts/', postView.PostList, name="authorPosts"),
+    path('author/<str:author_uuid>/posts/<str:post_uuid>/', postView.PostDetail, name="authorPost"),
 
-#     # Comment Endpoints
-#     path('author/<str:author_uuid>/posts/<str:post_uuid>/comments/', commentView.CommentList, name='commentList'),
-#     path('author/<str:author_uuid>/posts/<str:post_uuid>/comments/<str:comment_uuid>/', commentView.CommentDetail, name='commentDetail'),
+    # Comment Endpoints
+    path('author/<str:author_uuid>/posts/<str:post_uuid>/comments/', commentView.CommentList, name='commentList'),
+    path('author/<str:author_uuid>/posts/<str:post_uuid>/comments/<str:comment_uuid>/', commentView.CommentDetail, name='commentDetail'),
 
-#     # Liked Endpoints
-#     path('author/<str:author_uuid>/liked/', likedView.LikedList, name="likedList"),
+    # Liked Endpoints
+    path('author/<str:author_uuid>/liked/', likedView.LikedList, name="likedList"),
 
-#     # Like Endpoints
-#     path('author/<str:author_uuid>/posts/<str:post_uuid>/likes/', likeView.LikeListPost, name="likeListPost"),
-#     path('author/<str:author_uuid>/posts/<str:post_uuid>/comments/<str:comment_uuid>/likes/', likeView.LikeListComment, name="likeListComment"),
+    # Like Endpoints
+    path('author/<str:author_uuid>/posts/<str:post_uuid>/likes/', likeView.LikeListPost, name="likeListPost"),
+    path('author/<str:author_uuid>/posts/<str:post_uuid>/comments/<str:comment_uuid>/likes/', likeView.LikeListComment, name="likeListComment"),
 
-#     # Token Endpoints
-#     path('api-auth/', include('rest_framework.urls')),
-#     path('api/token/', TokenObtainPairView.as_view()),
-#     path('api/token/refresh/', TokenRefreshView.as_view()),
-# ]
+    # Token Endpoints
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+]
 
--- network/urls.py
+++ network/urls.py
@@ -1,60 +1,8 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.contrib import admin
-from rest_framework import routers
-from django.views.generic import TemplateView
-from api.views import index, authorView, followerView, postView, authView, inboxView, likeView, likedView, commentView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# router = routers.DefaultRouter()
-# router.register(r'api', views.TodoView, 'api')
 
 urlpatterns = [
-    # path('service/',include('api.urls')),
+    path('service/',include('api.urls')),
     path('admin/', admin.site.urls),
-    
-    # re_path('.*', TemplateView.as_view(template_name='index.html')),
-    
-
-    # Index
-    # path('', index.index, name="index"),
-    
-    # Auth Endpoints
-    path('service/author/login/', authView.LoginView.as_view(), name="login"),
-    path('service/author/register/', authView.SignupView, name="register"),
-
-    # Author Endpoints
-    path('service/authors/',authorView.AuthorList, name ='authorList'),
-    path('service/author/<str:author_uuid>/', authorView.AuthorDetail, name="authorDetail"),
-
-    # Inbox Endpoint
-    path('service/author/<str:author_uuid>/inbox/', inboxView.InboxList, name="inboxList"),
-
-    # Follower Endpoints
-    path('service/author/<str:author_uuid>/followers/', followerView.FollowerList, name="followerList"),
-    path('service/author/<str:author_uuid>/followers/<str:follower_uuid>/', followerView.FollowerDetail, name="followerDetail"),
-
-    # Post Endpoints
-    # path('service/author/<str:authorID>/stream/', postView.getStreamPosts, name="streamPosts"),
-    path('service/author/<str:author_uuid>/posts/', postView.PostList, name="authorPosts"),
-    path('service/author/<str:author_uuid>/posts/<str:post_uuid>/', postView.PostDetail, name="authorPost"),
-
-    # Comment Endpoints
-    path('service/author/<str:author_uuid>/posts/<str:post_uuid>/comments/', commentView.CommentList, name='commentList'),
-    path('service/author/<str:author_uuid>/posts/<str:post_uuid>/comments/<str:comment_uuid>/', commentView.CommentDetail, name='commentDetail'),
-
-    # Liked Endpoints
-    path('service/author/<str:author_uuid>/liked/', likedView.LikedList, name="likedList"),
-
-    # Like Endpoints
-    path('service/author/<str:author_uuid>/posts/<str:post_uuid>/likes/', likeView.LikeListPost, name="likeListPost"),
-    path('service/author/<str:author_uuid>/posts/<str:post_uuid>/comments/<str:comment_uuid>/likes/', likeView.LikeListComment, name="likeListComment"),
-
-    # Token Endpoints
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
-
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
-
 ]
-
