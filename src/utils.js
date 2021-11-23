3d659c7073c3642ee49ff9040ccae0360efa5487
Ayo Akindele <a.akindele01@gmail.com>
2021-11-22 22:07:00 -0700

    update utils helpers

--- src/utils.js
+++ src/utils.js
@@ -18,47 +18,52 @@ export const setObjectFromApi = (path, setObject) => {
           // if the token is not valid
           if (apiResponse.code === 'token_not_valid') {
 
-            // try to get a new token
-            fetch(`${getBackEndHostWithSlash()}service/api/token/refresh/`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                'refresh': `Bearer ${localStorage.getItem('refresh')}`
-              })
-            })
-              .then((corsResponse) => {
-                const apiPromise = corsResponse.json();
-                console.log(corsResponse)
-                console.log(apiPromise)
-                apiPromise.then((apiResponse) => {
-                  console.log(apiResponse)
+            const host = getBackEndHostWithSlash()
 
-                  // if getting a new token did not work
-                  if (apiResponse.code === 'token_not_valid') {
-                    // remove the token
-                    localStorage.removeItem('token')
-                  } else {
-                    // update the token
-                    localStorage.setItem('token', apiResponse.access)
-                    fetch(path, {
-                      method: 'GET',
-                      headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${localStorage.getItem('token')}`
-                      }
-                    })
-                      .then((corsResponse) => {
-                        const apiPromise = corsResponse.json();
-                        apiPromise.then((apiResponse) => {
-                          console.log(apiResponse)
-                          setObject(apiResponse);
-                        })
-                      })
-                  }
+            if (host !== null) {
+              // try to get a new token
+              fetch(`${host}service/api/token/refresh/`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  'refresh': `Bearer ${localStorage.getItem('refresh')}`
                 })
               })
+                .then((corsResponse) => {
+                  const apiPromise = corsResponse.json();
+                  console.log(corsResponse)
+                  console.log(apiPromise)
+                  apiPromise.then((apiResponse) => {
+                    console.log(apiResponse)
+
+                    // if getting a new token did not work
+                    if (apiResponse.code === 'token_not_valid') {
+                      // remove the token
+                      localStorage.removeItem('token')
+                    } else {
+                      // update the token
+                      localStorage.setItem('token', apiResponse.access)
+                      fetch(path, {
+                        method: 'GET',
+                        headers: {
+                          'Content-Type': 'application/json',
+                          'Authorization': `Bearer ${localStorage.getItem('token')}`
+                        }
+                      })
+                        .then((corsResponse) => {
+                          const apiPromise = corsResponse.json();
+                          apiPromise.then((apiResponse) => {
+                            console.log(apiResponse)
+                            setObject(apiResponse);
+                          })
+                        })
+                    }
+                  })
+                })
+
+            }
           } else {
             setObject(apiResponse);
           }
@@ -68,52 +73,60 @@ export const setObjectFromApi = (path, setObject) => {
 
 // validate the token
 export const validateToken = () => {
-  // get a resource from the backend
-  fetch(`${getBackEndHostWithSlash()}service/authors/`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    })
-      .then((corsResponse) => {
-        const apiPromise = corsResponse.json();
-        console.log(corsResponse)
-        console.log(apiPromise)
-        apiPromise.then((apiResponse) => {
-          console.log(apiResponse)
+  const firstHost = getBackEndHostWithSlash()
 
-          // if the token is not valid
-          if (apiResponse.code === 'token_not_valid') {
-
-            // try to get a new token
-            fetch(`${getBackEndHostWithSlash()}service/api/token/refresh/`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                'refresh': `Bearer ${localStorage.getItem('refresh')}`
-              })
-            })
-              .then((corsResponse) => {
-                const apiPromise = corsResponse.json();
-                apiPromise.then((apiResponse) => {
-
-                  // if getting a new token did not work
-                  if (apiResponse.code === 'token_not_valid') {
-                    // remove the token
-                    localStorage.removeItem('token')
-                  } else {
-                    // update the token
-                    localStorage.setItem('token', apiResponse.access)
-                  }
-                })
-              })
-          }
-        })
+  if (firstHost !== null) {
+    // get a resource from the backend
+    fetch(`${firstHost}service/authors/`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
       })
+        .then((corsResponse) => {
+          const apiPromise = corsResponse.json();
+          console.log(corsResponse)
+          console.log(apiPromise)
+          apiPromise.then((apiResponse) => {
+            console.log(apiResponse)
+
+            // if the token is not valid
+            if (apiResponse.code === 'token_not_valid') {
+
+              const secondHost = getBackEndHostWithSlash()
+
+              if (secondHost !== null) {
+                // try to get a new token
+                fetch(`${getBackEndHostWithSlash()}service/api/token/refresh/`, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    'refresh': `Bearer ${localStorage.getItem('refresh')}`
+                  })
+                })
+                  .then((corsResponse) => {
+                    const apiPromise = corsResponse.json();
+                    apiPromise.then((apiResponse) => {
+
+                      // if getting a new token did not work
+                      if (apiResponse.code === 'token_not_valid') {
+                        // remove the token
+                        localStorage.removeItem('token')
+                      } else {
+                        // update the token
+                        localStorage.setItem('token', apiResponse.access)
+                      }
+                    })
+                  })
+              }
+            }
+          })
+        })
+    }
 }
 
 // get host (e.g. "https://plurr.herokuapp.com/")
@@ -121,7 +134,7 @@ export const getFrontEndHostWithSlash = () => {
   if ((window !== null) && (window !== undefined)) {
     const host = window.location.href.split("/").slice(0, 4).join("/")
     return (host.includes("localhost") || host.includes("127.0.0.1")) 
-      ? "http://127.0.0.1:3000/" : host
+      ? "http://127.0.0.1:3000/" : host.endsWith("/") ? host : null
   }
   
   return null;
@@ -132,7 +145,7 @@ export const getBackEndHostWithSlash = () => {
   if ((window !== null) && (window !== undefined)) {
     const host = window.location.href.split("/").slice(0, 4).join("/")
     return (host.includes("localhost") || host.includes("127.0.0.1")) 
-      ? "http://127.0.0.1:8000/" : host
+      ? "http://127.0.0.1:8000/" : host.endsWith("/") ? host : null
   }
   
   return null;
